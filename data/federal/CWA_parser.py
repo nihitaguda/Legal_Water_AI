fd354f3630999ded89b7a745d5c4440789c2f4e6
Nihita Guda <nihita.guda@gmail.com>
2025-08-03 22:47:41 -0400

    parser issue

--- data/federal/CWA_parser.py
+++ data/federal/CWA_parser.py
@@ -1,18 +1,12 @@
 #open file
 #read line
 #see if like if in list of sections. add to file
-# if so, pull the contents from that point to editorial notes
+# if so, pull the contents from that point to editorial notes. if there areny any editorial notes, do it till the next section
 # run data cleaner 
 # add to new file
 # output file
 
 with open('CleanWaterAct.txt') as f:
-    print(f.read())
-    lines = f.readlines()
-    for i in range(len(lines) - 1):
-        if lines[i+1].strip().lower() == "section 1267":
-            i+=i
-            print(i)
-            if lines[i].strip().lower() == "Editorial Notes"
 
 
+ 
