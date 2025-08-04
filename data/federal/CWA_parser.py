5248be498c6003ea13e02031bccc765c1395f55b
Nihita Guda <nihita.guda@gmail.com>
2025-08-04 16:27:07 -0400

    first version of parser

--- data/federal/CWA_parser.py
+++ data/federal/CWA_parser.py
@@ -1,12 +1,19 @@
-#open file
-#read line
-#see if like if in list of sections. add to file
-# if so, pull the contents from that point to editorial notes. if there areny any editorial notes, do it till the next section
-# run data cleaner 
-# add to new file
-# output file
+section_names = ["section 1267", "section 1281"]
+extracted_texts = []
+current_lines = []
 
-with open('CleanWaterAct.txt') as f:
+with open("CleanWaterAct.txt", "r") as file:
+    count = 0
+    for line in file:
+        count += 1
+        strip = line.strip().lower()
+        if strip in section_names:
+            extracted_texts.append("\n".join(current_lines))
+            current_lines = []
+        current_lines.append(line)
 
+# If the file ends and we were still collecting, save the last section
+if (len(section_names) == len(extracted_texts)-1):
+    extracted_texts.append("\n".join(current_lines))
 
- 
+print("Collected section count:", len(extracted_texts))
